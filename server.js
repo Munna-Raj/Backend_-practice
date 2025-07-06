c91ebe3601ab4d74e7014f30c404e7d820f4f9b3
Raj Yadav <139562595+Raj-art73@users.noreply.github.com>
2025-07-06 22:17:27 +0545

    Day 1: Practicing setup of Express backend with MongoDB and dotenv

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./config/mongodb.js";
+
+
+dotenv.config();
+
+connectDB();         // mongoDB
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors());
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+    console.log(`Backend URI: ${process.env.BACKEND_URI}`);
+});
